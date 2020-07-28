05dd58d8f719f78e511d4fd2b41b7bf86ff90857
rifanid98 <adninsijawa.office@gmail.com>
2020-07-28 12:55:08 +0700

    add new screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,17 @@
 import Home from './Home';
 import Maps from './Maps';
 import Realtime from './Realtime';
+import Splash from './Splash';
+import Chat from './Chat';
+import Profile from './Profile';
 
-export { Home, Maps, Realtime };
+export {
+  Home,
+  Maps,
+  Realtime,
+  Splash,
+  Chat,
+  Profile
+};
+
+export * from './Auth'
