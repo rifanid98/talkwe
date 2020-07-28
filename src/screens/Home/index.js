7ee97376b9014cd52207c4f065f4a861f10f7706
rifanid98 <adninsijawa.office@gmail.com>
2020-07-28 12:54:21 +0700

    home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,24 +1,87 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { mainStyles as styles } from 'assets';
+import React, { Component } from 'react'
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { globalStyles as global, homeStyles as home } from 'assets';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faMapMarkedAlt, faUser } from '@fortawesome/free-solid-svg-icons'
+import { ScrollView } from 'react-native-gesture-handler';
 
-const Home = (props) => {
-  return (
-    <>
-      <View style={styles.container}>
-        <Text
-          style={[styles.button, styles.buttonPrimary]}
-          onPress={() => props.navigation.navigate('maps')}>
-          Maps
-        </Text>
-        <Text
-          style={[styles.button, styles.buttonPrimary]}
-          onPress={() => props.navigation.navigate('realtime')}>
-          Realtime
-        </Text>
-      </View>
-    </>
-  );
-};
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <View style={[home.container, global.relative]}>
+          {/* header */}
+          <View style={home.header}>
+            <Text style={home.title}>Messages</Text>
+            <View style={home.menu}>
+              <Text style={home.menuIcon} onPress={() => this.props.navigation.navigate('maps')}> <FontAwesomeIcon icon={faMapMarkedAlt} size={20} /> </Text>
+              <Text style={home.menuIcon} onPress={() => this.props.navigation.navigate('profile')}> <FontAwesomeIcon icon={faUser} size={20} /> </Text>
+            </View>
+          </View>
+          {/* friends list */}
+          <View style={home.friendsList}>
+            <Text style={home.label}>Friends List</Text>
+            <ScrollView
+              style={home.friendsListItems}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{
+                paddingRight: 25
+              }}
+            >
+              {
+                [1, 1, 1, 1, 1, 1, 1, 1].map((data, index) => {
+                  return (
+                    <View style={home.friendsListItem}>
+                      <Image
+                        key={index}
+                        style={home.friendsListItemImage}
+                        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+                      />
+                      <Text style={home.friendsListItemName}>Anne</Text>
+                    </View>
+                  )
+                })
+              }
+            </ScrollView>
+          </View>
+          {/* messages */}
+          <View style={home.messagesList}>
+            <Text style={home.label}>Messages List</Text>
+            <ScrollView
+              style={home.messagesListItems}
+              // showsVerticalScrollIndicator={false}
+            >
+              {
+                [1, 1, 1, 1, 1, 1, 1, 1].map((data, index) => {
+                  return (
+                    <TouchableOpacity
+                      key={index}
+                      style={home.messagesListItem}
+                      onPress={() => this.props.navigation.navigate('chat')}
+                    >
+                      <Image
+                        style={home.messagesListItemImage}
+                        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+                      />
+                      <View style={home.messagesListItemContent}>
+                        <Text style={home.messagesListItemContentTitle}>Anne</Text>
+                        <Text style={home.messagesListItemContentText}>Lorem ipsum dolor sit .</Text>
+                      </View>
+                      <View style={home.messagesListItemInfo}>
+                        <Text style={home.messagesListItemInfoTitle}>12min</Text>
+                        <Text style={home.messagesListItemInfoCircle}>2</Text>
+                      </View>
+                    </TouchableOpacity>
+                  )
+                })
+              }
+            </ScrollView>
+          </View>
+        </View>  
+      </>
+    )
+  }
+}
 
 export default Home;
