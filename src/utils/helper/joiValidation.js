3a183d2ee8edb613bde4f609f1f7d743dbc04f58
rifanid98 <adninsijawa.office@gmail.com>
2020-07-28 23:46:06 +0700

    add profile validation schema

--- src/utils/helper/joiValidation.js
+++ src/utils/helper/joiValidation.js
@@ -16,3 +16,7 @@ export const registerSchema = Joi.object({
   email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
 });
+export const profileSchema = Joi.object({
+  full_name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+});
