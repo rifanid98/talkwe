3dbdcfd10c9cb9fa0a67170571e429ad7eb03c7e
rifanid98 <adninsijawa.office@gmail.com>
2020-07-28 12:54:55 +0700

    splash screen

---
+++ src/screens/Splash/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Logo } from 'components';
+
+class Splash extends Component {
+  render() {
+    return (
+      <>
+        <Logo width={20} height={20}/>
+      </>
+    )
+  }
+}
+
+export default Splash;
