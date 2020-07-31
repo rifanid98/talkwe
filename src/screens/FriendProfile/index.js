c45f3288a8fa07fd82e7a1686ed0fbfae508071d
rifanid98 <adninsijawa.office@gmail.com>
2020-07-31 20:43:10 +0700

    add getAddress function

--- src/screens/FriendProfile/index.js
+++ src/screens/FriendProfile/index.js
@@ -4,7 +4,8 @@ import { globalStyles as global, profileStyles as profile } from 'assets';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faArrowLeft, faMapMarkerAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { connect } from 'react-redux';
-import { logout, getDetailUser } from 'modules';
+import { logout, getDetailUser, getAddress } from 'modules';
+
 
 class FriendProfile extends Component {
   constructor(props) {
@@ -13,14 +14,15 @@ class FriendProfile extends Component {
     }
   }
   componentDidMount() {
-   this.getUserInfo()
+    this.getUserInfo()
+    this.getAddress()
  }
   getUserInfo = () => {
     const token = this.props.auth.data.tokenLogin;
     const friendID = this.props.route.params.senderID;
     this.props.getDetailUser(token, friendID)
       .then(res => {
-        
+
       })
       .catch(error => {
         console.log(error)
@@ -35,6 +37,16 @@ class FriendProfile extends Component {
         // Alert.alert('Update Profile Failed', errorMessage)
       });
   }
+  getAddress = () => {
+    const latlang = this.props.users.data[0].location;
+    this.props.getAddress(latlang)
+      .then(res => {
+
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
   render() {
     return (
       <>
@@ -46,32 +58,42 @@ class FriendProfile extends Component {
           {/* profile */}
           <View style={profile.profile}>
             {
-              this.props.users.isLoading
+              this.props.users && this.props.users.isLoading
                 ? <Text>Loading</Text>
-                : (
-                  <>
-                    <Image
-                      style={profile.image}
-                      source={{ uri: this.props.users.data[0].image }}
-                    />
-                    <Text style={profile.name}>{this.props.users.data[0].full_name}</Text>
-                    <Text style={profile.email}>
-                      <FontAwesomeIcon icon={faEnvelope} />
-                      {this.props.users.data[0].email}
-                    </Text>
-                    <Text style={profile.location}>
-                      <FontAwesomeIcon icon={faMapMarkerAlt} />
-                      {this.props.users.data[0].location}
-                    </Text>
-                  </>
-                )
+                : this.props.users.data && this.props.users.data.length > 0
+                  ? (
+                    <>
+                      <Image
+                        style={profile.image}
+                        source={{ uri: this.props.users.data[0].image }}
+                      />
+                      <Text style={profile.name}>{this.props.users.data[0].full_name}</Text>
+                      <Text style={profile.email}>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                        {this.props.users.data[0].email}
+                      </Text>
+                      <Text style={profile.location}>
+                        <FontAwesomeIcon icon={faMapMarkerAlt} />
+                        {
+                          this.props.location.isLoading
+                            ? <Text>Loading...</Text>
+                            : this.props.location.data
+                              ? this.props.location.data.display_name
+                              : <Text
+                                onPress={() => this.getAddress()}
+                              >Cannot get location. Touch to refresh.</Text>
+                        } 
+                      </Text>
+                    </>
+                  )
+                  : <Text>An error occured</Text>
             }
-            
+
           </View>
           {/* setting */}
           <View style={profile.lists}>
           </View>
-         </View>  
+        </View>  
       </>
     )
   }
@@ -79,12 +101,14 @@ class FriendProfile extends Component {
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
-  users: state.users
+  users: state.users,
+  location: state.location
 })
 
 const mapDispatchToProps = {
   logout,
-  getDetailUser
+  getDetailUser,
+  getAddress
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(FriendProfile);
