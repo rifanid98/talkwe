aab28ae00de4bf6b05924b070c5553e631c7b76f
rifanid98 <adninsijawa.office@gmail.com>
2020-07-31 20:35:30 +0700

    add origin url

--- src/configs/appConfig.js
+++ src/configs/appConfig.js
@@ -1,14 +1,20 @@
-import { API_URL, ACTIVE_CONFIG } from '@env';
+import { API_URL, ACTIVE_CONFIG, ACTIVE_ORIGIN } from '@env';
 const activeConfig = ACTIVE_CONFIG;
-const origin = 'localhost:8081';
+const activeOrigin = ACTIVE_ORIGIN;
 // const origin = window.location.origin;
 
+const origin = {
+  wifi: '192.168.43.81',
+  lan: '192.168.42.15',
+};
+const originConfig = origin[activeOrigin]
+const port = 3000;
 const constants = {
   dev: {
     url: {
-      api: 'http:///192.168.42.15:3000/talkwe/api/v1',
+      api: `http://192.168.42.15:3000/talkwe/api/v1`,
       assets: '../assets',
-      origin: origin,
+      origin: `http://192.168.42.15:3000`,
     },
   },
 
