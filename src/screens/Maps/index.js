f8c05735e477d1f0b800de6e6485170ef30576ed
rifanid98 <adninsijawa.office@gmail.com>
2020-08-03 13:54:30 +0700

    add getFriendsList & gotoFriendCoordinates function

--- src/screens/Maps/index.js
+++ src/screens/Maps/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text, ScrollView, Image, Platform, PermissionsAndroid, Dimensions } from 'react-native'
-import { globalStyles as global, mapsStyles as maps } from 'assets';
+import { View, Text, Image, Platform, PermissionsAndroid, Dimensions } from 'react-native'
+import { globalStyles as global, mapsStyles as maps, font, colorScheme as color } from 'assets';
 import MapView, { Marker } from 'react-native-maps';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import Geolocation from 'react-native-geolocation-service';
+import { getFriendsList } from 'modules';
+import { connect } from 'react-redux';
+import { LoadingIcon, FriendsList, UsersList } from 'components';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
 
 class Maps extends Component {
   constructor(props) {
@@ -29,8 +35,27 @@ class Maps extends Component {
   async componentDidMount() {
     await this.requestPermissions()
     this.findCoordinates()
+    this.getFriendsList()
   }
 
+  /**
+   * API Services
+   */
+  getFriendsList = () => {
+    const token = this.props.auth.data.tokenLogin;
+    const id = this.props.auth.data.id
+    token
+      ? this.props.getFriendsList(token, id)
+        .then((res) => {
+          this.setState({
+            ...this.state,
+            friendsList: this.props.friends.data
+          })
+        }).catch((error) => {
+          console.log(error, `get friends lists failed`)
+        })
+      : console.log(`cannot find token`)
+  }
   /**
    * Geolocation API
    */
@@ -72,6 +97,24 @@ class Maps extends Component {
   };
 
   /**
+   * Logics
+   */
+  goToFriendCoordinates = (coords) => {
+    const latitude = parseFloat(coords.split(',')[0]);
+    const longitude = parseFloat(coords.split(',')[1]);
+    this.setState({
+      ...this.state,
+      location: {
+        ...this.state.location,
+        latitude: latitude,
+        longitude: longitude
+      }
+    }, () => {
+      this.mapView.animateToRegion(this.state.location, 2000);
+    })
+  };
+
+  /**
    * DOM Render
    */
   render() {
@@ -97,80 +140,66 @@ class Maps extends Component {
           }}
           showsUserLocation={true}
         >
-          <Marker
-            draggable
-            coordinate={this.state.location}
-            title="My Location"
-            description="This is your current position"
-          />
+          {
+            this.props.friends.data && this.props.friends.data.length > 0
+              && this.props.friends.data.map(friend => {
+                return (
+                  <Marker
+                    coordinate={{
+                      latitude: parseFloat(friend.location.split(',')[0]),
+                      longitude: parseFloat(friend.location.split(',')[1])
+                    }}
+                    title={friend.full_name.split(' ')[0]}
+                    description={`${friend.full_name}'s position`}
+                  >
+                    <Image source={{ uri: friend.image }} style={maps.markerImage} />
+                  </Marker>
+                )
+              })
+          }
         </MapView>
-        {
-          this.state.location.latitude !== 0 && this.state.location.longitude !== 0
-            ? (
-              <>
-              </>
-            )
-            : <Text>Loading</Text>
-        }
-        {/* panel */}
-        {
-          this.state.togglePanel
-            ? (
-              <>
-                <View style={maps.panel}>
-                  <TouchableOpacity
-                    style={maps.panelButton}
-                    onPress={() => this.setState({ ...this.state, togglePanel: !this.state.togglePanel })}
-                  >
-                    <View style={maps.line}></View>
-                  </TouchableOpacity>
-                  <Text style={maps.label}>NEARBY FRIENDS LIST</Text>
-                  <ScrollView
-                    style={maps.listItems}
-                    showsVerticalScrollIndicator={false}
-                  >
-                    {
-                      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,].map((data, index) => {
-                        return (
-                          <TouchableOpacity
-                            key={index}
-                            style={maps.listItem}
-                            onPress={() => this.props.navigation.navigate('profile')}
-                          >
-                            <Image
-                              style={maps.itemImage}
-                              source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-                            />
-                            <View style={maps.itemContent}>
-                              <Text style={maps.name}>Anne</Text>
-                              <Text style={maps.status}>My status is here!</Text>
-                            </View>
-                            <View style={maps.itemAction}>
-                              <Text style={maps.distance}>1KM</Text>
-                              <Text style={maps.button}>ADD</Text>
-                            </View>
-                          </TouchableOpacity>
-                        )
-                      })}
-                  </ScrollView>
-                </View>
-              </>
-            )
-            : (
-              <>
-                <TouchableOpacity
-                  style={maps.panelButtonToggle}
-                  onPress={() => this.setState({ ...this.state, togglePanel: !this.state.togglePanel })}
-                >
-                  <View style={maps.line}></View>
-                </TouchableOpacity>
-              </>
-            )
-        }
-         </View>
+        <View style={{
+          flex: 1
+        }}>
+        {/* <View style={maps.panel}> */}
+          <Tab.Navigator
+            initialRouteName="friendsList"
+            backBehavior="initialRoute"
+            tabBarOptions={{
+              activeTintColor: color.accent,
+              inactiveTintColor: 'grey',
+              labelStyle: {
+                fontSize: 20,
+                fontFamily: font.heading
+              },
+              pressColor: color.accent,
+              indicatorStyle: {
+                backgroundColor: color.accent
+              }
+            }}
+            lazy={true}
+            removeClippedSubviews={true}
+          >
+            <Tab.Screen name="friendsList" options={{ title: 'FRIENDS LIST' }}>
+              {() => <FriendsList goToFriendCoordinates={(coords) => this.goToFriendCoordinates(coords)} />}
+            </Tab.Screen>
+            <Tab.Screen name="usersList" options={{ title: 'USERS LIST' }}>
+              {() => <UsersList goToFriendCoordinates={(coords) => this.goToFriendCoordinates(coords)} />}
+            </Tab.Screen>
+          </Tab.Navigator>
+        </View>
+      </View>
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  friends: state.friends,
+})
 
-export default Maps;
+const mapDispatchToProps = {
+  getFriendsList,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Maps);
