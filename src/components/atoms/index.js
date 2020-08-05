0ff490b5cd8c18e0168f34e87e8c55fc50e78b55
rifanid98 <adninsijawa.office@gmail.com>
2020-08-05 16:09:04 +0700

    add BadgeOnlineStatus component

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -2,10 +2,12 @@ import Logo from './Logo'
 import LoadingButton from './LoadingButton'
 import LoadingIcon from './LoadingIcon'
 import BadgeNotification from './BadgeNotification'
+import BadgeOnlineStatus from './BadgeOnlineStatus'
 
 export {
   Logo,
   LoadingButton,
   LoadingIcon,
-  BadgeNotification
+  BadgeNotification,
+  BadgeOnlineStatus
 }
