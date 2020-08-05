aa1a8ec05384bbefe15f3e563d0859b901f7ea5d
rifanid98 <adninsijawa.office@gmail.com>
2020-08-05 16:13:55 +0700

    get friend info is realtime now & use BadgeOnlineStatus component

--- src/screens/FriendProfile/index.js
+++ src/screens/FriendProfile/index.js
@@ -5,24 +5,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faArrowLeft, faMapMarkerAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { connect } from 'react-redux';
 import { logout, getDetailUser, getAddress } from 'modules';
-
+import io from 'socket.io-client';
+import { appConfig } from 'configs';
+import { BadgeOnlineStatus } from 'components';
 
 class FriendProfile extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      isMount: false
     }
   }
+
+  /**
+   * LifeCycles
+   */
   componentDidMount() {
     this.getUserInfo()
     this.getAddress()
- }
+    this._socketio()
+  }
+
+  /**
+   * API Services
+   */
   getUserInfo = () => {
     const token = this.props.auth.data.tokenLogin;
     const friendID = this.props.route.params.senderID;
     this.props.getDetailUser(token, friendID)
       .then(res => {
-
+        this.setState({
+          ...this.state,
+          isMount: true
+        })
       })
       .catch(error => {
         console.log(error)
@@ -38,14 +53,26 @@ class FriendProfile extends Component {
       });
   }
   getAddress = () => {
-    const latlang = this.props.users.data[0].location;
-    this.props.getAddress(latlang)
-      .then(res => {
+    if (this.props.users.data.length > 0) {
+      const latlang = this.props.users.data[0].location;
+      this.props.getAddress(latlang)
+        .then(res => {
 
-      })
-      .catch(error => {
-        console.log(error)
-      })
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+
+  /**
+   * Socket.IO Services
+   */
+  _socketio = () => {
+    this.socket = io(appConfig.url.origin);
+    this.socket.on('refresh', () => {
+      this.getUserInfo()
+    });
   }
   render() {
     return (
@@ -58,49 +85,45 @@ class FriendProfile extends Component {
           {/* profile */}
           <View style={profile.profile}>
             {
-              this.props.users && this.props.users.isLoading
+              this.props.users.isLoading && this.props.isMount === false
                 ? <Text>Loading</Text>
-                : this.props.users.data && this.props.users.data.length > 0
-                  ? (
-                    <>
-                      <Image
-                        style={profile.image}
-                        source={{ uri: this.props.users.data[0].image }}
-                      />
-                      <View style={{
-                        flexDirection: 'row',
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                      }}>
-                        {
-                          this.props.users.data[0].online === 1 && <View style={{
-                            height: 15,
-                            width: 15,
-                            backgroundColor: 'lightgreen',
-                            borderRadius: 100,
-                            marginRight: 12
-                          }}></View>
-                        }
-                        <Text style={profile.name}>{this.props.users.data[0].full_name}</Text>
-                      </View>
-                      <Text style={profile.email}>
-                        <FontAwesomeIcon icon={faEnvelope} />
-                        {this.props.users.data[0].email}
-                      </Text>
-                      <Text style={profile.location}>
-                        <FontAwesomeIcon icon={faMapMarkerAlt} />
-                        {
-                          this.props.location.isLoading
-                            ? <Text>Loading...</Text>
-                            : this.props.location.data
-                              ? this.props.location.data.display_name
-                              : <Text
-                                onPress={() => this.getAddress()}
-                              >Cannot get location. Touch to refresh.</Text>
-                        } 
-                      </Text>
-                    </>
-                  )
+                : this.props.users.data 
+                  ? this.props.users.data.length > 0
+                    ? (
+                      <>
+                        <Image
+                          style={profile.image}
+                          source={{ uri: this.props.users.data[0].image }}
+                        />
+                        <View style={{
+                          flexDirection: 'row',
+                          justifyContent: 'center',
+                          alignItems: 'center'
+                        }}>
+                          {
+                            this.props.users.data[0].online === 1 && <BadgeOnlineStatus height={15} width={15} color="lightgreen" />
+                          }
+                          <Text style={profile.name}>{this.props.users.data[0].full_name}</Text>
+                        </View>
+                        <Text style={profile.email}>
+                          <FontAwesomeIcon icon={faEnvelope} />
+                          {this.props.users.data[0].email}
+                        </Text>
+                        <Text style={profile.location}>
+                          <FontAwesomeIcon icon={faMapMarkerAlt} />
+                          {
+                            this.props.location.isLoading
+                              ? <Text>Loading...</Text>
+                              : this.props.location.data
+                                ? this.props.location.data.display_name
+                                : <Text
+                                  onPress={() => this.getAddress()}
+                                >Cannot get location. Touch to refresh.</Text>
+                          }
+                        </Text>
+                      </>
+                    )
+                    : <Text>An error occured</Text>
                   : <Text>An error occured</Text>
             }
 
