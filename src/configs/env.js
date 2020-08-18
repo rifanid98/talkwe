ecd6d7ce4711ef90828a9f5219bbccd0d8679dd9
rifanid98 <adninsijawa.office@gmail.com>
2020-08-18 08:24:57 +0700

    -

--- src/configs/env.js
+++ src/configs/env.js
@@ -2,5 +2,5 @@ export const ACTIVE_CONFIG = 'dev';
 export const ACTIVE_THEME = 'primary';
 export const API_URL = `http://192.168.42.15:3000/talkwe/api/v1`;
 export const ORIGIN = `http://192.168.42.15:3000`;
-export const GOOGLE_API_KEY = `AIzaSyBBc1rtAoRDWm1j-dAAxvGu2SWQ2qaYHcg`;
-export const LOCATION_IQ_PRIVATE_KEY = `64e977ad7c9336`;
+export const GOOGLE_API_KEY = ``;
+export const LOCATION_IQ_PRIVATE_KEY = ``;
