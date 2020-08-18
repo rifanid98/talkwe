a734de51b3cb468725e720b8f5c1030799e603fb
rifanid98 <adninsijawa.office@gmail.com>
2020-08-18 07:35:20 +0700

    update url api

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,6 +1,6 @@
 export const ACTIVE_CONFIG = 'dev';
 export const ACTIVE_THEME = 'primary';
-export const API_URL = `http://localhost:3000/talkwe/api/v1`;
-export const ORIGIN = `http://localhost:3000`;
+export const API_URL = `http://34.207.84.36/talkwe/api/v1`;
+export const ORIGIN = `http://34.207.84.36`;
 export const GOOGLE_API_KEY = ``;
 export const LOCATION_IQ_PRIVATE_KEY = ``;
